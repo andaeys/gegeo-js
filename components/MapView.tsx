ff845a4dd5a251d3bb9161e65d06422124c301e1
andaeys <ekayudha.sulistianda@quipper.com>
2024-05-10 10:30:00 +0700

    implement show spatial data on map view

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl'
+import styles from './MapViewer.module.css'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFwZXBlcyIsImEiOiJjbHZ6emp1YmUzM3VjMmpteG1iZ21namhyIn0.NOdtJVHNGuQMPlS--dNGRQ';
 
@@ -15,7 +16,6 @@ const MapViewer = ({ onClose, geoJSONData }: { onClose: () => void; geoJSONData:
         map.on('load', () => {
             const geoJSON = JSON.parse(geoJSONData);
 
-            // Calculate the bounding box of the GeoJSON features
             const bounds = new mapboxgl.LngLatBounds();
 
             geoJSON.features.forEach((feature) => {
@@ -36,7 +36,6 @@ const MapViewer = ({ onClose, geoJSONData }: { onClose: () => void; geoJSONData:
                 });
             }
 
-            // Add GeoJSON data to the map
             map.addSource('geojson-data', {
                 type: 'geojson',
                 data: geoJSON,
@@ -52,31 +51,17 @@ const MapViewer = ({ onClose, geoJSONData }: { onClose: () => void; geoJSONData:
             });
         });
 
-        // Clean up resources when the component unmounts
         return () => {
             map.remove();
         };
     }, [geoJSONData]);
 
-    const getBounds = (geoJSON: any) => {
-        const coordinates = geoJSON.features.reduce((acc: any[], feature: any) => {
-            return acc.concat(feature.geometry.coordinates);
-        }, []);
-
-        const bounds = coordinates.reduce((acc: any[], coord: any) => {
-            return [
-                [Math.min(acc[0][0], coord[0]), Math.min(acc[0][1], coord[1])],
-                [Math.max(acc[1][0], coord[0]), Math.max(acc[1][1], coord[1])]
-            ];
-        }, [[Infinity, Infinity], [-Infinity, -Infinity]]);
-
-        return bounds;
-    };
-
     return (
-        <div className="map-dialog">
-            <div id="map" className="map"></div>
-            <button className="close-button" onClick={onClose}>Close</button>
+        <div className={styles['map-viewer-wrapper']}>
+            <div id="map" className={styles.map}></div>
+            <button className={styles['close-button']} onClick={onClose}>
+                <span className="material-icons">close</span>
+            </button>
         </div>
     );
 };
